fa86644caacc6ad3566c298538e783d27072b4dc
YB <ybcsie@gmail.com>
2018-08-08 01:56:23 +0800

    fix problem: same file name when publish and unpublish

--- update.js
+++ update.js
@@ -1,8 +1,8 @@
 var path = require('path'),
-  moment = require('moment'),
-  hfm = require('hexo-front-matter'),
-  fs = require('hexo-fs'),
-  extend = require('extend');
+	moment = require('moment'),
+	hfm = require('hexo-front-matter'),
+	fs = require('hexo-fs'),
+	extend = require('extend');
 //  yfm = util.yfm,
 //  escape = util.escape;
 
@@ -17,79 +17,80 @@ var path = require('path'),
  * @param {Function} callback
  */
 module.exports = function (model, id, update, callback, hexo) {
-  function removeExtname(str) {
-    return str.substring(0, str.length - path.extname(str).length);
-  }
-  var post = hexo.model(model).get(id)
-  if (!post) {
-    return callback('Post not found');
-  }
-  var config = hexo.config,
-    slug = post.slug = hfm.escape(post.slug || post.title, config.filename_case),
-    layout = post.layout = (post.layout || config.default_layout).toLowerCase(),
-    date = post.date = post.date ? moment(post.date) : moment();
+	function removeExtname(str) {
+		return str.substring(0, str.length - path.extname(str).length);
+	}
+	var post = hexo.model(model).get(id)
+	if (!post) {
+		return callback('Post not found');
+	}
+	var config = hexo.config,
+		slug = post.slug = hfm.escape(post.slug || post.title, config.filename_case),
+		layout = post.layout = (post.layout || config.default_layout).toLowerCase(),
+		date = post.date = post.date ? moment(post.date) : moment();
 
-  var split = hfm.split(post.raw),
-    frontMatter = split.data
-    compiled = hfm.parse([frontMatter, '---', split.content].join('\n'));
+	var split = hfm.split(post.raw),
+		frontMatter = split.data
+	compiled = hfm.parse([frontMatter, '---', split.content].join('\n'));
 
-  var preservedKeys = ['title', 'date', 'tags', 'categories', '_content', 'author'];
-  Object.keys(hexo.config.metadata || {}).forEach(function (key) {
-    preservedKeys.push(key);
-  });
-  var prev_full = post.full_source,
-    full_source = prev_full;
-  if (update.source && update.source !== post.source) {
-    // post.full_source only readable ~ see: /hexo/lib/models/post.js
-    full_source = hexo.source_dir + update.source
-  }
+	var preservedKeys = ['title', 'date', 'tags', 'categories', '_content', 'author'];
+	Object.keys(hexo.config.metadata || {}).forEach(function (key) {
+		preservedKeys.push(key);
+	});
+	var prev_full = post.full_source,
+		full_source = prev_full;
+	if (update.source && update.source !== post.source) {
+		// post.full_source only readable ~ see: /hexo/lib/models/post.js
+		full_source = hexo.source_dir + update.source
+		full_source = fs.ensurePathSync(full_source);
+	}
 
-  preservedKeys.forEach(function (attr) {
-    if (attr in update) {
-      compiled[attr] = update[attr]
-    }
-  });
-  compiled.date = moment(compiled.date).toDate()
+	preservedKeys.forEach(function (attr) {
+		if (attr in update) {
+			compiled[attr] = update[attr]
+		}
+	});
+	compiled.date = moment(compiled.date).toDate()
 
-  delete update._content
-  var raw = hfm.stringify(compiled);
-  update.raw = raw
-  update.updated = moment()
+	delete update._content
+	var raw = hfm.stringify(compiled);
+	update.raw = raw
+	update.updated = moment()
 
-  // tags and cats are only getters now. ~ see: /hexo/lib/models/post.js
-  if ( typeof update.tags !== 'undefined' ) {
-    post.setTags(update.tags)
-    delete update.tags
-  }
-  if ( typeof update.categories !== 'undefined' ) {
-    post.setCategories(update.categories)
-    delete update.categories
-  }
+	// tags and cats are only getters now. ~ see: /hexo/lib/models/post.js
+	if (typeof update.tags !== 'undefined') {
+		post.setTags(update.tags)
+		delete update.tags
+	}
+	if (typeof update.categories !== 'undefined') {
+		post.setCategories(update.categories)
+		delete update.categories
+	}
 
-  extend(post, update)
+	extend(post, update)
 
-  post.save(function () {
-  //  console.log(post.full_source, post.source)
-    fs.writeFile(full_source, raw, function(err){
-      if (err) return callback(err);
+	post.save(function () {
+		//  console.log(post.full_source, post.source)
+		fs.writeFile(full_source, raw, function (err) {
+			if (err) return callback(err);
 
-      if (full_source !== prev_full) {
-        fs.unlinkSync(prev_full)
-        // move asset dir
-        var assetPrev = removeExtname(prev_full);
-        var assetDest = removeExtname(full_source);
-        fs.exists(assetPrev).then(function(exist) {
-          if (exist) {
-            fs.copyDir(assetPrev, assetDest).then(function() {
-              fs.rmdir(assetPrev);
-            });
-          }
-        });
-      }
-      hexo.source.process([post.source]).then(function () {
-  //      console.log(post.full_source, post.source)
-        callback(null, hexo.model(model).get(id));
-      });
-    });
-  });
+			if (full_source !== prev_full) {
+				fs.unlinkSync(prev_full)
+				// move asset dir
+				var assetPrev = removeExtname(prev_full);
+				var assetDest = removeExtname(full_source);
+				fs.exists(assetPrev).then(function (exist) {
+					if (exist) {
+						fs.copyDir(assetPrev, assetDest).then(function () {
+							fs.rmdir(assetPrev);
+						});
+					}
+				});
+			}
+			hexo.source.process([post.source]).then(function () {
+				//      console.log(post.full_source, post.source)
+				callback(null, hexo.model(model).get(id));
+			});
+		});
+	});
 }
